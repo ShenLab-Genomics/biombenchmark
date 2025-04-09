5c7094f1e2946efb741517ffad541a86d9d68149
SilverN <893325022@qq.com>
2025-04-09 13:54:49 +0800

    Update analyzer
    update analyzer

--- analyzer/parse_output.py
+++ analyzer/parse_output.py
@@ -5,44 +5,72 @@ import numpy as np
 import pandas as pd
 
 
+import argparse
+import sys
+import numpy as np
+import os
+import pandas as pd
+import re
+
+
 class DataProcessor:
+    # 分为以下几块：初始化，预处理（筛选行，读取组别），数据处理，数据写入，数据保存
     def __init__(self, filename):
         self.filename = filename
         self.base_name = os.path.splitext(filename)[0]
         self.output_filename = f"{self.base_name}_collected_data.csv"
-        self.parameters = ['method', 'Epoch']
         self.data_frame = None
+        self.df = pd.DataFrame()
 
-    def read_and_process_data(self):
+    def preprogress(self):
+        with open(self.filename, 'r') as data:
+            lines = data.readlines()
+        # 读取行，找到以Epoch（小组别）开头的行
+        Methods = []
         data_list = []
+        parameters = ['Method', 'Epoch']
+        n = 4
 
-        with open(self.filename, 'r') as infile:
-            for line in infile:
-                if line.startswith('Epoch'):
-                    self.current_epoch = line.strip()
-                elif line.startswith('Test') and self.current_epoch is not None:
-                    self.process_line(line, self.current_epoch, data_list)
+        # processline(lines[]# from "Epoch" to end, Methods , data_list, parameters)
+        # featureline = lines[n+1]
+        for line_index, line in enumerate(lines):
+            if line.startswith('Epoch:'):
+                try:
+                    self.processline(
+                        lines[line_index: line_index + n],  data_list, parameters)
+                except IndexError:
+                    # 如果出现索引超出范围的错误，打印错误信息并继续处理下一条记录
+                    print(
+                        f"Skipping data due to index out of range at line {line_index}")
+                    continue  # 继续下一次循环"""
         # 将数据列表转换为Pandas DataFrame
-        self.data_frame = pd.DataFrame(data_list, columns=self.parameters)
+        self.data_frame = pd.DataFrame(data_list, columns=parameters)
 
-    def process_line(self, line, epoch, data_list):
-        rest_of_line = line[4:].lstrip('\t')
-        parts = rest_of_line.split()
-        method = parts[0]
-        data_part = rest_of_line.replace(method, '', 1).strip()
+    def processline(self, line_list, data_list, parameters):
 
-        row_data = {'method': method}
-        key_value_pairs = [item.split(': ')
-                           for item in data_part.split('\t') if item]
-        for key, value in key_value_pairs:
-            key = key.strip()
-            if key:
-                row_data[key] = value
-                if key not in self.parameters:
-                    self.parameters.append(key)
-        # 将当前的 Epoch 信息添加到行数据中
-        row_data['Epoch'] = epoch.split(': ')[1]
-        data_list.append(row_data)
+        subfeature = line_list[1]
+
+        # 处理准确率等数值
+        for featureline in line_list[2:]:
+            # 标注Epoch序号
+            Epoch = line_list[0].split(': ')[-1].strip("\n")
+            row_data = {'Epoch': Epoch}
+            try:
+                # 去除分号和换行符
+                featureline = re.sub(r'[;\n]', ' ', str(featureline))
+                # 每行第二项是Method，第三项是Stage，第四项开始是各个指标
+                features = featureline.split("\t")
+                row_data['Method'] = features[1]
+            except IndexError:
+                print(line_list)
+            row_data['Stage'] = features[2]
+            evaluate_set = features[3:]
+            for feature in evaluate_set:
+                key, value = feature.split(':')
+                row_data[key] = value.strip()
+            data_list.append(row_data)
+            # 更新参数列表
+            parameters.extend(key for key in row_data if key not in parameters)
 
     def save_data_to_csv(self, output=None):
         # 保存 DataFrame 到 CSV 文件
@@ -72,7 +100,7 @@ if __name__ == "__main__":
     full_path = args.input
     if os.path.exists(full_path):
         processor = DataProcessor(full_path)
-        processor.read_and_process_data()
+        processor.preprogress()
         processor.save_data_to_csv(args.output)
         print("Data collection and processing is complete.")
     else:
